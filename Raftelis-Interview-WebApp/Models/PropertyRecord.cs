84511ad4138e3317c8371a557971ba9552305fe1
sborcherding66 <sborcherding66@gmail.com>
2024-03-26 13:59:15 -0700

    Initial commit with project files

---
+++ Raftelis-Interview-WebApp/Models/PropertyRecord.cs
@@ -0,0 +1,16 @@
+﻿namespace Raftelis_Interview_WebApp.Models
+{
+    public class PropertyRecord
+    {
+        public string Pin { get; set; }
+        public string Address { get; set; }
+        public string Owner { get; set; }
+        public decimal MarketValue { get; set; }
+        public DateTime SaleDate { get; set; }
+        public decimal SalePrice { get; set; }
+        public string Link { get; set; }
+
+        // Property for Google Maps link
+        public string GoogleMapsLink => $"https://www.google.com/maps/search/?api=1&query={Uri.EscapeDataString(Address)}";
+    }
+}
---
+++ Raftelis-Interview-WebApp/Services/PropertyDataService.cs
@@ -0,0 +1,37 @@
+﻿// Services/PropertyDataService.cs
+using Raftelis_Interview_WebApp.Models;
+using System.Globalization;
+using System.IO; // Make sure to include this for File operations
+
+namespace Raftelis_Interview_WebApp.Services
+{
+    public class PropertyDataService
+    {
+        public static List<PropertyRecord> LoadPropertyData()
+        {
+            // Construct the path to the data file.
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Parcels.txt");
+
+            var records = new List<PropertyRecord>();
+            var lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines.Skip(1)) // first line is headers
+            {
+                var columns = line.Split('|');
+                var record = new PropertyRecord
+                {
+                    Pin = columns[0],
+                    Address = columns[1],
+                    Owner = columns[2],
+                    MarketValue = decimal.Parse(columns[3], CultureInfo.InvariantCulture),
+                    SaleDate = DateTime.ParseExact(columns[4], "M/d/yyyy", CultureInfo.InvariantCulture),
+                    SalePrice = decimal.Parse(columns[5], CultureInfo.InvariantCulture),
+                    Link = columns[6]
+                };
+                records.Add(record);
+            }
+
+            return records;
+        }
+    }
+}
